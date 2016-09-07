02306c55c107802ae1fcfa28c3bf9a8b31ec09d6
ryotamac <tennis.suta6@gmail.com>
2016-09-07 17:51:07 +0900

    webpack_without_tsloader_babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  entry: './src/main/app.js',
+  entry: './src/babel/app.js',
   output: {
-    filename: './bundle/bundle.js.withOutTsloader'
+    filename: './bundle/bundle.js.withOutTsloaderBabel'
   },
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['', '.webpack.js', '.js']
+    extensions: ['', '.webpack.js', '.js', '.jsx']
   },
   module: {
     loaders: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { 
+      {
         test: /\.ts(x)?$/,
         loader: 'babel?presets[]=es2015!ts-loader',
         exclude: /node_modules/
